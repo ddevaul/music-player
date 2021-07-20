bbd5b7682077222b89ffd28ead568285ff6fb423
Desi <desi.devaul@gmail.com>
2021-07-19 22:23:47 -0400

    configuring for heroku deployment

--- spotify_backend/spotify_backend/settings.py
+++ spotify_backend/spotify_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,3 +156,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = (
 #        'http://localhost:4200',
 # )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
